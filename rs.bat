4821b99aa6360abc20a3ea813940b1dc8d4e6c29
Konfekt <Konfekt@users.noreply.github.com>
2022-04-13 14:45:05 +0200

    rs: accept additional arguments passed to rsync

--- rs.bat
+++ rs.bat
@@ -1,6 +1,9 @@
 @ECHO OFF
 SETLOCAL
 
+CALL :SKIPLASTTWOARGS ALLBUTLASTTWO_ARGS %*
+IF "%ALLBUTLASTTWO_ARGS%"=="~1" SET ALLBUTLASTTWO_ARGS=
+
 call :PENULTIMATE_ARG %*
 SET FROM_FOLDER=%PENULTIMATE_ARG%
 call :ULTIMATE_ARG %*
@@ -10,7 +13,9 @@ call :DOS2UNIX %FROM_FOLDER% FROM_FOLDER_UNIX
 call :DOS2UNIX %TO_FOLDER% TO_FOLDER_UNIX
 
 SET RSYNC_ARGS=--archive --hard-links --executability --one-file-system --compress --delete --modify-window=1 --human-readable --info=stats1,progress2
-rsync %RSYNC_ARGS% %FROM_FOLDER_UNIX% %TO_FOLDER_UNIX%
+rsync %RSYNC_ARGS% %ALLBUTLASTTWO_ARGS% %FROM_FOLDER_UNIX% %TO_FOLDER_UNIX%
+
+rem {{{ HELPER FUNCTIONS
 
 :PENULTIMATE_ARG
 set "PENULTIMATE_ARG=%~f1"
@@ -32,3 +37,21 @@ SET UNIXDIR=/cygdrive/%UNIXSLASHDIR::=%
 
 SET %~2=%UNIXDIR%
 goto :EOF
+
+rem From https://stackoverflow.com/a/5807218/7485823
+rem Return all but last two args in variable given in %1
+:SKIPLASTTWOARGS returnvar args ...
+    SETLOCAL
+        SET $return=%1
+        SET SKIP_LAST_ARG=
+        SHIFT
+    :skiplasttwoargs_2
+        IF NOT "%~3"=="" SET "SKIP_LAST_ARG=%SKIP_LAST_ARG% %1"
+        SHIFT
+        IF NOT "%~2"=="" GOTO skiplasttwoargs_2
+    ENDLOCAL&CALL SET "%$return%=%SKIP_LAST_ARG:~1%"
+GOTO :EOF
+
+rem }}}
+
+:: ex: set foldmethod=marker: 
